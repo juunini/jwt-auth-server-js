68edbdc31518222c95175facd232737b0c437433
Juunini <juuni.ni.i@gmail.com>
2025-01-28 14:33:07 +0900

    init project

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export function jwtAuthInit() {
+  console.log('jwtAuthInit');
+}
+
+export async function jwtAuthLogin() {
+  console.log('jwtAuthLogin');
+}
+
+export async function jwtAuthValidation() {
+  console.log('jwtAuthValidation');
+}
+
+export async function jwtAuthRefresh() {
+  console.log('jwtAuthRefresh');
+}
+
+export async function jwtAuthLogout() {
+  console.log('jwtAuthLogout');
+}
